57426936e0d6eb9b4d46d7fd8c6a352b8dc7b7bd
tdutton <tdutton@Ubuntu.myguest.virtualbox.org>
2023-08-23 21:31:30 -0500

    Implement generic command execution

--- main.c
+++ main.c
@@ -5,71 +5,52 @@
 #include <dirent.h>
 #include <unistd.h>
 
-int getPathDirectories(char***);
+int splitString(char* str, char delimiter, char*** tokens);
 
 int main() {
-	// bool running = true;
-	// char line[2000] = {0};
+	bool running = true;
+	char line[2000] = {0};
 
 
-	// while (running) {
-	// 	printf("# "); // Print shell prompt
-	// 	gets(line, sizeof(line));
+	while (running) {
+		printf("# "); // Print shell prompt
+		gets(line, sizeof(line));
 
-	// 	if (line[0] == 0) { // Catch EOF characters
-	// 		running = false
-	// 	} else { // Parse commands
-		
-	// 	}
+		if (line[0] == 0) { // Catch EOF characters
+			running = false;
+		} else { // Parse commands
+			char** tokens;
+			splitString(line, ' ', &tokens);
 
-	// 	memset(line, 0, sizeof(line)); // Clear previous command
-	// }
+			char* args[1] = {NULL};
+			execvp(tokens[0], args);
+		}
 
-	char** path_dirs;
-	int num_dirs = getPathDirectories(&path_dirs);
-
-	for (int i = 0; i < num_dirs; i++) {
-		DIR* dir = opendir(path_dirs[i]);
-	
-		struct dirent* entry;
-		while ((entry = readdir(dir)) != NULL) {
-			if (strcmp(entry->d_name, "top") == 0) {
-				printf("File found in %s: %s\n", path_dirs[i], entry->d_name);
-				closedir(dir);
-			}
-    	}
+		memset(line, 0, sizeof(line)); // Clear previous command
 	}
 }
 
-int getPathDirectories(char*** path_dirs) {
-	// Stores directories found in the PATH environment variable in the provided pointer
-	// Returns -1 if retrieving directories failed, otherwise the number of directories
-	char* path = getenv("PATH");
-
-	if (path == NULL) {return -1;} // Failed
-
-	// Count number of path directories
-	int num_dirs = 1;
-	for (int i = 0; path[i]; i++) {
-		if (path[i] == ':') {num_dirs++;}
+int splitString(char* str, char delimiter, char*** tokens) {
+	// Count number of tokens
+	int num_tokens = 1;
+	for (int i = 0; str[i]; i++) {
+		if (str[i] == delimiter) {str++;}
 	}
 
 	// Allocate an array to store directories
-	*path_dirs = (char**) malloc(num_dirs * sizeof(char*));
+	*tokens = (char**) malloc(num_tokens * sizeof(char*));
 
 
-	// Split path into individual directories
-	char* token = strtok(path, ":");
+	// Split string into individual tokens
+	char* token = strtok(str, &delimiter);
 
-	// Store directories
 	int i = 0;
 	while (token != NULL) {
-		(*path_dirs)[i] = (char*) malloc(sizeof(token));
-		strcpy((*path_dirs)[i], token);
-		printf("%s\n", (*path_dirs)[i]);
+		(*tokens)[i] = (char*) malloc(sizeof(token));
+		strcpy((*tokens)[i], token);
 		i++;
-		token = strtok(NULL, ":");
+		token = strtok(NULL, &delimiter);
 	}
 
-	return num_dirs;
+	return num_tokens;
 }
