d0f7805c4d29467094591251b994ba9e87a17dd0
tdutton <tdutton@Ubuntu.myguest.virtualbox.org>
2023-08-30 00:58:04 -0500

    Job Structure

--- main.c
+++ main.c
@@ -10,28 +10,47 @@
 #include <fcntl.h>
 #include <signal.h>
 
-void executeCommand(char** args, int input_fd, int output_fd, int error_fd);
+#define MAX_JOBS 20
+#define MAX_LINE_LENGTH 200
 
-char** parseCommand(char** tokens, int num_tokens, int* input_fd, int* output_fd, int* error_fd);
+typedef struct
+{
+	// Job information
+	pid_t pgid;
+	bool foreground;
+	bool status; // Running or Stopped
+	char command[MAX_LINE_LENGTH];
+
+	// Process information
+	int num_commands;
+	char** commands[2];
+	int command_length[2];
+	int input_fd[2];
+	int output_fd[2];
+	int error_fd[2];
+} Job;
+
+Job parseLine(char* line);
 
 int splitString(char* str, const char* delimiter, char*** tokens);
 
-void sigint_handler(int signum);
+void executeJob(Job job);
 
-void sigtstp_handler(int signum);
+void signalHandler(int signum);
+
 
 pid_t child_pid = -1;
 
 int main() {
 	// Assign signal handlers
-	signal(SIGINT, sigint_handler);
-	// signal(SIGTSTP, sigint_handler);
+	signal(SIGINT, signalHandler);
+	signal(SIGTSTP, signalHandler);
 
 	const char whitespace[2] = " ";
 	const char pipechar[2] = "|";
 
 	bool running = true;
-	char line[2000] = {0};
+	char line[MAX_LINE_LENGTH] = {0};
 
 	while (running) {
 		printf("# "); // Print shell prompt
@@ -40,45 +59,16 @@ int main() {
 		if (line[0] == 0) { // Catch EOF characters
 			running = false;
 		} else {
-			// Remove newline character from the end of the line
-			for (int i = 0; line[i]; i++) {
-				if (line[i] == '\n' && line[i + 1] == '\0') {
-					line[i--] = '\0';
+			Job job = parseLine(line);
+			
+			executeJob(job);
+
+			// Free job commands
+			for (int i = 0; i < job.num_commands; i++) {
+				for (int j = 0; j < job.command_length[i]; j++) {
+					free(job.commands[i][j]);
 				}
-			}
-
-			// Split the line on a pipe
-			char** cmds;
- 			int num_cmds = splitString(line, pipechar, &cmds);
-
-			int pipe_fd[2]; // 0 - Read, 1 - Write
-
-			if (num_cmds == 2) {
-				pipe(pipe_fd);
-			}
-
-			for (int i = 0; i < num_cmds; i++) {
-				// Split the line on every whitepsace
-				char** tokens;
-				int num_tokens = splitString(cmds[i], whitespace, &tokens);
-
-				// Set default file descriptors
-				int input_fd = (i == 1 && num_cmds == 2) ? pipe_fd[0] : STDIN_FILENO;
-				int output_fd = (i == 0 && num_cmds == 2) ? pipe_fd[1] : STDOUT_FILENO;
-				int error_fd = STDERR_FILENO;
-
-				// Parse command from tokens
-				char** args = parseCommand(tokens, num_tokens, &input_fd, &output_fd, &error_fd);
-
-				// Execute command
-				executeCommand(args, input_fd, output_fd, error_fd);
-
-				// Free tokens and args
-				for (int j = 0; j < num_tokens; j++) {
-					free(tokens[j]);
-				}
-				free(tokens);
-				free(args);
+				free(job.commands[i]);
 			}
 		}
 
@@ -86,81 +76,104 @@ int main() {
 	}
 }
 
-void executeCommand(char** args, int input_fd, int output_fd, int error_fd) {
-	pid_t pid = fork();
-	if (pid == -1) {
-		perror("yash");
-	} else if (pid == 0) { // Child process
-		// Redirect file descriptors
-		dup2(input_fd, STDIN_FILENO);
-		dup2(output_fd, STDOUT_FILENO);
-		dup2(error_fd, STDERR_FILENO);
+Job parseLine(char* line) {
+	Job job;
 
-		// Execute command
-		execvp(args[0], args);
+	job.foreground = true;
+	job.status = true;
+	strcpy(job.command, line);
 
-		// If this point has been reached an error has occurred
-		perror("yash");
-	} else { // Parent process
-		child_pid = pid;
-
-		wait(NULL);
-
-		child_pid = -1;
-	}
-
-	// Close file descriptors if they were changed
-	if (input_fd != STDIN_FILENO) {
-		close(input_fd);
-	}
-	if (output_fd != STDOUT_FILENO) {
-		close(output_fd);
-	}
-	if (error_fd != STDERR_FILENO) {
-		close(error_fd);
-	}
-}
-
-char** parseCommand(char** tokens, int num_tokens, int* input_fd, int* output_fd, int* error_fd) {
-	int cmd_length = num_tokens;
-
-	// Check for redirection symbols
-	for (int i = 0; i < num_tokens; i++) {
-		if (strcmp(tokens[i], "<") == 0) { // Input
-			*input_fd = open(tokens[i + 1], O_RDONLY);
-
-			if (*input_fd == -1) {
-				perror("yash");
-			} else if (i < cmd_length) {
-				cmd_length = i;
-			}
-		} else if (strcmp(tokens[i], ">") == 0) { // Output
-			*output_fd = open(tokens[i + 1], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH);
-			
-			if (*output_fd == -1) {
-				perror("yash");
-			} else if (i < cmd_length) {
-				cmd_length = i;
-			}
-		} else if (strcmp(tokens[i], "2>") == 0) { // Error
-			*error_fd = open(tokens[i + 1], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH);
-
-			if (*error_fd == -1) {
-				perror("yash");
-			} else if (i < cmd_length) {
-				cmd_length = i;
-			}
+	// Remove newline character from the end of the line
+	for (int i = 0; line[i]; i++) {
+		if (line[i] == '\n' && line[i + 1] == '\0') {
+			line[i--] = '\0';
 		}
 	}
 
-	// Extract command arguments from tokens
-	char** args = (char**) malloc((cmd_length + 1) * sizeof(char*));
-	for (int i = 0; i < cmd_length; i++) {
-		args[i] = tokens[i];
+	// Check for an '&' indicating a background process
+	for (int i = 0; line[i]; i++) {
+		if (line[i] == '&' && line[i + 1] == '\0') {
+			job.foreground = false;
+			line[i--] = '\0';
+		}
 	}
-	args[cmd_length] = NULL;
 
-	return args;
+	// Split the line on a pipe
+	char** commands;
+	int num_commands = splitString(line, "|", &commands);
+	job.num_commands = num_commands;
+
+	int pipe_fd[2]; // 0 - Read, 1 - Write
+	if (num_commands == 2) {
+		pipe(pipe_fd);
+	}
+
+	for (int i = 0; i < num_commands; i++) {
+		// Split the line on every whitepsace
+		char** command_tokens;
+		int num_command_tokens = splitString(commands[i], " ", &command_tokens);
+
+		// Set default file descriptors
+		job.input_fd[i] = (i == 1 && num_commands == 2) ? pipe_fd[0] : STDIN_FILENO;
+		job.output_fd[i] = (i == 0 && num_commands == 2) ? pipe_fd[1] : STDOUT_FILENO;
+		job.error_fd[i] = STDERR_FILENO;
+
+		int cmd_length = num_command_tokens;
+
+		// Check for redirection symbols
+		for (int j = 0; j < num_command_tokens; j++) {
+			if (strcmp(command_tokens[j], "<") == 0) { // Input
+				job.input_fd[i] = open(command_tokens[j + 1], O_RDONLY);
+
+				if (job.input_fd[i] == -1) {
+					perror("yash");
+				} else if (j < cmd_length) {
+					cmd_length = j;
+				}
+			} else if (strcmp(command_tokens[j], ">") == 0) { // Output
+				job.output_fd[i] = open(command_tokens[j + 1], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH);
+				
+				if (job.output_fd[i] == -1) {
+					perror("yash");
+				} else if (j < cmd_length) {
+					cmd_length = j;
+				}
+			} else if (strcmp(command_tokens[j], "2>") == 0) { // Error
+				job.error_fd[i] = open(command_tokens[j + 1], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH);
+
+				if (job.error_fd[i] == -1) {
+					perror("yash");
+				} else if (j < cmd_length) {
+					cmd_length = j;
+				}
+			}
+		}
+
+		// Extract command arguments from tokens
+		job.commands[i] = (char**) malloc((cmd_length + 1) * sizeof(char*));
+		for (int j = 0; j < cmd_length; j++) {
+			job.commands[i][j] = (char*) malloc(strlen(command_tokens[j]) * sizeof(char));
+			strcpy(job.commands[i][j], command_tokens[j]);
+		}
+		job.commands[i][cmd_length] = NULL;
+
+		job.command_length[i] = cmd_length;
+		
+		// Free command tokens
+		for (int j = 0; j < num_command_tokens; j++) {
+			free(command_tokens[j]);
+		}
+		free(command_tokens);
+	}
+
+	// Free commands
+	for (int i = 0; i < num_commands; i++) {
+		free(commands[i]);
+	}
+	free(commands);
+
+
+	return job;
 }
 
 int splitString(char* str, const char* delimiter, char*** tokens) {
@@ -188,9 +201,50 @@ int splitString(char* str, const char* delimiter, char*** tokens) {
     return num_tokens;
 }
 
-void sigint_handler(int signum) {
-    if (child_pid != -1) {
-        // Kill the child process if it's running
-        kill(child_pid, SIGINT);
+void executeJob(Job job) {
+	for (int i = 0; i < job.num_commands; i++) {
+		pid_t pid = fork();
+
+		if (pid == -1) {
+			perror("yash");
+		} else if (pid == 0) { // Child process
+			// Redirect file descriptors
+			dup2(job.input_fd[i], STDIN_FILENO);
+			dup2(job.output_fd[i], STDOUT_FILENO);
+			dup2(job.error_fd[i], STDERR_FILENO);
+
+			// Execute command
+			execvp(job.commands[i][0], job.commands[i]);
+
+			// If this point has been reached an error has occurred
+			perror("yash");
+		} else { // Parent process
+			child_pid = pid;
+
+			waitpid(pid, NULL, WUNTRACED);
+
+			child_pid = -1;
+		}
+
+		// Close file descriptors if they were changed
+		if (job.input_fd[i] != STDIN_FILENO) {
+			close(job.input_fd[i]);
+		}
+		if (job.output_fd[i] != STDOUT_FILENO) {
+			close(job.output_fd[i]);
+		}
+		if (job.error_fd[i] != STDERR_FILENO) {
+			close(job.error_fd[i]);
+		}
+	}
+}
+
+void signalHandler(int signum) {
+    if (child_pid == -1) {
+		return;
     }
+
+	if (signum == SIGINT || signum == SIGTSTP) {
+		kill(child_pid, signum);
+	}
 }
